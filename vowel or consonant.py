99aaed451ef74f6ddf73ef7a237bf170ff06b3c3
devachandar <47187469+devachandar@users.noreply.github.com>
2019-02-02 17:57:33 +0530

    Update vowel or consonant.py

--- vowel or consonant.py
+++ vowel or consonant.py
@@ -1,6 +1,9 @@
 y=input()
-x=y.lower()
-if x=='a' or x=='e' or x=='i' or x=='o' or x=='u':
-  print("Vowel")
+if y. isalpha():
+  x=y.lower()
+  if x=='a' or x=='e' or x=='i' or x=='o' or x=='u':
+    print("Vowel")
+  else:
+    print("Consonant")
 else:
-  print("Consonant")  
+  print("Invalid") 
